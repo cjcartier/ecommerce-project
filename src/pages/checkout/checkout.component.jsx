9769bb03ec09f16eb6f956ba6c89830b473d57fe
Curtis Cartier <curtiscartier@curtiss-mbp.lan>
2021-02-28 23:44:13 -0800

    Created collections

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -11,35 +11,38 @@ import {
 
 import './checkout.styles.scss';
 
-const CheckoutPage = ({ cartItems, total }) => (
-	<div className='checkout-page'>
-		<div className='checkout-header'>
-			<div className='header-block'>
-				<span>Product</span>
+const CheckoutPage = ({ cartItems, total }) =>
+	cartItems.length ? (
+		<div className='checkout-page'>
+			<div className='checkout-header'>
+				<div className='header-block'>
+					<span>Product</span>
+				</div>
+				<div className='header-block'>
+					<span>Description</span>
+				</div>
+				<div className='header-block'>
+					<span>Quantity</span>
+				</div>
+				<div className='header-block'>
+					<span>Price</span>
+				</div>
+				<div className='header-block'>
+					<span>Remove</span>
+				</div>
 			</div>
-			<div className='header-block'>
-				<span>Description</span>
-			</div>
-			<div className='header-block'>
-				<span>Quantity</span>
-			</div>
-			<div className='header-block'>
-				<span>Price</span>
-			</div>
-			<div className='header-block'>
-				<span>Remove</span>
+
+			{cartItems.map((cartItem) => (
+				<CheckoutItem key={cartItem.id} cartItem={cartItem} />
+			))}
+
+			<div className='total'>
+				<span>${total}</span>
 			</div>
 		</div>
-
-		{cartItems.map((cartItem) => (
-			<CheckoutItem key={cartItem.id} cartItem={cartItem} />
-		))}
-
-		<div className='total'>
-			<span>${total}</span>
-		</div>
-	</div>
-);
+	) : (
+		<div>Empty Cart</div>
+	);
 
 const mapStateToProps = createStructuredSelector({
 	cartItems: selectCartItems,
--- src/redux/cart/cat.reducer.js
+++ src/redux/cart/cat.reducer.js
@@ -22,7 +22,7 @@ const cartReducer = (state = INITIAL_SATE, action) => {
 			return {
 				...state,
 				cartItems: state.cartItems.filter(
-					(cartItem) => cartItem.id != action.payload.id
+					(cartItem) => cartItem.id !== action.payload.id
 				),
 			};
 		case CartActionTypes.REMOVE_ITEM:
